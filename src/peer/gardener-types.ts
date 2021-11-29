d66421505febbb5571ef10831b0b320774f4d250
Cinnamon <cinnamon_bun_github@fastmail.com>
2021-11-29 07:44:05 -0800

    add gardener documentation comments

--- src/peer/gardener-types.ts
+++ src/peer/gardener-types.ts
@@ -3,13 +3,28 @@ import { IPeer, PeerId } from './peer-types';
 export type NetworkType =
     'HTTP_SERVER' |
     'HTTP_CLIENT';
+    // 'HYPERSWARM_PEER';
 
 export interface RemotePeer {
     peerId: PeerId,
     networkType: NetworkType,
+    lastSeen: number,  // unix timestamp
 }
 
 export interface IGardener {
+    /*
+        A Gardener is responsible for maintaining some of the remotePeer entries
+        in the localPeer.remotePeers map.
+
+        That map should be a subclass of Map that's observable (like SuperbusMap)
+            https://github.com/cinnamon-bun/superbus-map
+        so other parts of the code can notice peers coming and going.
+
+        The gardener may be a server in which case it should start listening for
+        connections when it hatches.
+        Or it may be a client in which case it does not listen, it only connects
+        to a preset list of URLs.
+    */
     networkType: NetworkType,
     localPeer: IPeer,
     hatch(): Promise<void>,
